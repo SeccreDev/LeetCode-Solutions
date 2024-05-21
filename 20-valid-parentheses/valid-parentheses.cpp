d2d4f9de50c6bc24e659d2bc1237e9ecc27a8c59
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-21 17:47:29 -0400

    Time: 3 ms (42.17%) | Memory: 7.6 MB (59.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        stack <char> tempCharacters;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                tempCharacters.push(s[i]);
+            }
+            else
+            {
+                if(!tempCharacters.empty())
+                {
+                    switch (s[i])
+                    {
+                        case ')':
+                            if (tempCharacters.top() == '(')
+                            {
+                                tempCharacters.pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                            break;
+
+                        case ']':
+                            if (tempCharacters.top() == '[')
+                            {
+                                tempCharacters.pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                            break;
+
+                        case '}':
+                            if (tempCharacters.top() == '{')
+                            {
+                                tempCharacters.pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                            break;
+                        default:
+                            return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(tempCharacters.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+};
