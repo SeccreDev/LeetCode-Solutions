d2af7a7e959e2c5febeabe20c1580f96a205707f
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-25 19:29:31 -0400

    Time: 118 ms (64.60%) | Memory: 105.4 MB (50.76%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -23,14 +23,15 @@ class Solution
             // }
             // return solution;
 
-            vector <int> solution(temps.size());
+            vector <int> solution(temps.size(), 0);
             stack <int> daysTillWarmer;
 
             for (int i = 0; i < temps.size(); i++)
             {
                 while(!daysTillWarmer.empty() && temps[i] > temps[daysTillWarmer.top()])
                 {
-                    solution[daysTillWarmer.top()] = i - daysTillWarmer.top();
+                    int indexStack = daysTillWarmer.top();
+                    solution[indexStack] = i - indexStack;
                     daysTillWarmer.pop();
                 }
                 daysTillWarmer.push(i);
