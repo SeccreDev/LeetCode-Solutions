c02a886f804b3a49826e0f9d8223f78e2b07806e
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-25 19:23:34 -0400

    Time: 139 ms (14.24%) | Memory: 105.5 MB (35.98%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,20 +3,41 @@ class Solution
     public:
         vector<int> dailyTemperatures(std::vector<int>& temps)
         {
-            vector <int> results(temps.size());
-            stack <int> stack;
+            // TIME LIMIT EXCEEDED
+            // vector <int> solution;
             
+            // for (int i = 0; i < temps.size(); i++)
+            // {
+            //     int temperature = temps[i];
+            //     int daysTillWarmer = 0;
+            //     for(int j = i; j < temps.size(); j++)
+            //     {
+            //         int temperature2 = temps[j];
+            //         if (temperature2 > temperature)
+            //         {
+            //             daysTillWarmer = j - i;
+            //             break;
+            //         }
+            //     }
+            //     solution.push_back(daysTillWarmer);
+            // }
+            // return solution;
+
+            vector <int> solution(temps.size());
+            stack <int> daysTillWarmer;
+
             for (int i = 0; i < temps.size(); i++)
             {
-                while (!stack.empty() && temps[stack.top()] < temps[i])
+                while(!daysTillWarmer.empty() && temps[i] > temps[daysTillWarmer.top()])
                 {
-                    results[stack.top()] = i - stack.top();
-                    stack.pop();
+                    solution[daysTillWarmer.top()] = i - daysTillWarmer.top();
+                    daysTillWarmer.pop();
                 }
-                stack.push(i);
+                daysTillWarmer.push(i);
             }
+            return solution;
 
-            return results;
+
         }
         
 };
