feeb7b230be9f4053428ab004a9843f29d25033f
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-25 19:30:39 -0400

    Time: 113 ms (76.47%) | Memory: 105.3 MB (50.76%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,26 +3,6 @@ class Solution
     public:
         vector<int> dailyTemperatures(std::vector<int>& temps)
         {
-            // TIME LIMIT EXCEEDED
-            // vector <int> solution;
-            
-            // for (int i = 0; i < temps.size(); i++)
-            // {
-            //     int temperature = temps[i];
-            //     int daysTillWarmer = 0;
-            //     for(int j = i; j < temps.size(); j++)
-            //     {
-            //         int temperature2 = temps[j];
-            //         if (temperature2 > temperature)
-            //         {
-            //             daysTillWarmer = j - i;
-            //             break;
-            //         }
-            //     }
-            //     solution.push_back(daysTillWarmer);
-            // }
-            // return solution;
-
             vector <int> solution(temps.size(), 0);
             stack <int> daysTillWarmer;
 
