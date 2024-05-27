0947c40648cef63361f1599de485a9ac0e293bd4
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-26 22:01:57 -0400

    Time: 105 ms (97.49%) | Memory: 82 MB (73.81%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,30 +1,29 @@
-class Solution {
-    class Car{
-        public:
-        int pos, speed;
-        Car(int p, int s):pos(p), speed(s) {};
-    };
-       static bool myComp(Car&a, Car&b) {
-           return a.pos < b.pos;
-       
-    }
-public:
-    int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<Car>cars;
-        for(int i=0; i<position.size(); ++i) {
-            Car car(position[i], speed[i]);
-            cars.push_back(car);
-        }
-        sort(cars.begin(), cars.end(), myComp);
+class Solution
+{
+    public:
+        int carFleet(int target, vector<int>& position, vector<int>& speed)
+        {
+            int size = speed.size();
+            vector<pair<int,int>> positionAndSpeed;
 
-        stack<float>st;
-        for(auto car:cars) {
-            float time = (target-car.pos) / ((float) car.speed);
-            while(!st.empty() && time >= st.top()) {
-                st.pop();
+            for(int i = 0; i < size; i++)
+            {
+                positionAndSpeed.push_back({position[i], speed[i]});
             }
-            st.push(time);
+
+            sort(positionAndSpeed.begin(), positionAndSpeed.end());
+
+            int amountOfCarFleets = 0;
+            double max= 0;
+            for(int k = size - 1; k >= 0; k--)
+            {
+                double time  = 1.0 * (target - positionAndSpeed[k].first) / positionAndSpeed[k].second;
+                if(time > max)
+                {
+                    amountOfCarFleets += 1;
+                    max = time;
+                }
+            } 
+            return amountOfCarFleets;  
         }
-        return st.size();
-    }
 };
