c8b5fa4e72257f06e76fdd51d455f3244eb1cb7d
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-29 22:12:35 -0400

    Time: 11 ms (36.86%) | Memory: 18 MB (19.59%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,20 @@
-class Solution
-{
-    public:
-        vector<int> twoSum(vector<int>& numbers, int target)
-        {
-            unordered_map <int, int> map;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
 
-            for(int i = 0; i < numbers.size(); i++)
-            {
-                int difference = target - numbers[i];
-                if(map.count(difference))
-                {
-                    return {map[difference]+1, i+1};
-                }
-                map[numbers[i]] = i;
+        while (left < right) {
+            int total = numbers[left] + numbers[right];
+
+            if (total == target) {
+                return {left + 1, right + 1};
+            } else if (total > target) {
+                right--;
+            } else {
+                left++;
             }
-            return {};
         }
+        return {-1, -1}; // If no solution is found        
+    }
 };
