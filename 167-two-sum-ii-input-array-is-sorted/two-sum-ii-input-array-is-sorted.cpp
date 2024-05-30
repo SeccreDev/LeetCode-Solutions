0bbdf9e9ddafdcfbb6b2feea948d24d15e156d9b
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-29 22:05:54 -0400

    Time: 12 ms (24.81%) | Memory: 19.2 MB (5.16%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        unordered_map <int, int> map;
+
+        for(int i = 0; i < numbers.size(); i++)
+        {
+            int difference = target - numbers[i];
+            if(map.count(difference))
+            {
+                return {map[difference]+1, i+1};
+            }
+            map[numbers[i]] = i;
+        }
+        return {};
+    }
+};
