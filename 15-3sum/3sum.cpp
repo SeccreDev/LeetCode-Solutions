b0add6669b85cc296dbc44af6019dbbc71473915
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-31 19:44:03 -0400

    Time: 58 ms (79.10%) | Memory: 27.10 MB (93.80%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -8,7 +8,6 @@ class Solution
 
             for(int i = 0; i < nums.size() - 2; i++)
             {
-
                 if(i > 0 && nums[i] == nums[i-1])
                 {
                     continue;
