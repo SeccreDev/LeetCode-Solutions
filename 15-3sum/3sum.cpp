30104dbd4e628bc2e6695463593060c228cfda98
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-31 19:41:49 -0400

    Update 3sum.cpp

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,29 +1,58 @@
-//Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
+class Solution
+{
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums)
+        {
+            sort(nums.begin(), nums.end());
+            vector <vector<int>> answer;
+
+            for(int i = 0; i < nums.size() - 2; i++)
+            {
+
+                if(i > 0 && nums[i] == nums[i-1])
+                {
+                    continue;
+                }
+
+                int next = i + 1;
+                int last = nums.size() - 1;
+
+                while  (next < last)
+                {
+                    int sum = nums[i] + nums[next] + nums[last];
+
+                    if (sum == 0)
+                    {
+                        // Triplet found
+                        answer.push_back({nums[i], nums[next], nums[last]});
+                        
+                        // Skipping duplicate elements for next and last
+                        while(next < last && nums[next] == nums[next + 1])
+                        {
+                            next++;
+                        }
+
+                        while(next < last && nums[last] == nums[last - 1])
+                        {
+                            last--;
+                        }
+
+                        // Update the pointers next and last
+                        next++;
+                        last--;
+                    }
+                    else if(sum < 0)
+                    {
+                        // Increment next to increase the sum
+                        next++;
+                    }
+                    else
+                    {
+                        // Decrement last to decrease the sum
+                        last--;
+                    }
                 }
             }
+            return answer;
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
-    }
 };
