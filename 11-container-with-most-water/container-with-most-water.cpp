acddbe29674ef24b0a81abe102ce40bd07b0d30f
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-05-31 23:49:37 -0400

    Time: 62 ms (54.54%) | Memory: 61.3 MB (92.13%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,25 +1,26 @@
-class Solution {
-public:
-    int maxArea(vector<int>& height)
-    {
-        int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
-
-        while (left < right)
+class Solution
+{
+    public:
+        int maxArea(vector<int>& height)
         {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
+            int left = 0;
+            int right = height.size() - 1;
+            int maxArea = 0;
 
-            if (height[left] < height[right])
+            while (left < right)
             {
-                left++;
-            } else
-            {
-                right--;
+                int currentArea = min(height[left], height[right]) * (right - left);
+                maxArea = max(maxArea, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                } else
+                {
+                    right--;
+                }
             }
-        }
 
-        return maxArea;
-    }
+            return maxArea;
+        }
 };
