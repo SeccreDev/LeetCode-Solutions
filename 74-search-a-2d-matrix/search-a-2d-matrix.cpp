bd4fb1699c7a5586236c1022486a44b345f86c74
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-12-11 23:19:10 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (16.31%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target)
     {
+        // Linear Search
+        /*
         bool found = false;
         for (int i = 0; i < matrix.size(); i++)
         {
@@ -14,5 +16,30 @@ public:
             }      
         }
         return found;
+        */
+
+        // Binary Search
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            int lowest = 0, highest = matrix[i].size() - 1;
+
+            while (lowest <= highest)
+            {
+                int middleValue = (lowest + highest) / 2;
+                if (matrix[i][middleValue] == target)
+                {
+                    return true;
+                }
+                else if (matrix[i][middleValue] < target)
+                {
+                    lowest = middleValue + 1;
+                }
+                else
+                {
+                    highest = middleValue - 1;
+                }
+            }
+        }
+        return false;
     }
 };
