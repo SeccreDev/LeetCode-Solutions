57e03628c7ff18e856c016cb153d47836c99c16f
SeccreDev <144842474+SeccreDev@users.noreply.github.com>
2024-12-11 23:13:36 -0500

    Time: 0 ms (100.00%) | Memory: 31.3 MB (15.78%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int search(vector<int>& nums, int target)
     {
+        // Linear Search
+        /*
         int foundAt = -1;
         for(int index = 0; index < nums.size(); index++)
         {
@@ -11,5 +13,27 @@ public:
             }
         }
         return foundAt;
+        */
+
+        // Binary Search
+        int lowest = 0, highest = nums.size() - 1;
+        while (lowest <= highest)
+        {
+            int middleValue = (lowest + highest) / 2;
+            if (nums[middleValue] == target)
+            {
+                return middleValue;
+            }
+            else if (nums[middleValue] < target)
+            {
+                lowest = middleValue + 1;
+            }
+            else
+            {
+                highest = middleValue - 1;
+            }
+        }
+        return -1;
+        
     }
 };
